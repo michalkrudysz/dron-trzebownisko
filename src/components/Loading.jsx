c95b3f7e4d0d84c92940f300e8885432abc761a3
Michal <michal@drontrzebownisko.pl>
2024-02-28 13:35:11 +0100

    Created component with loading animation and styled.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,34 @@
 import classes from "./Loading.module.scss";
+import droneBody from "../assets/loading/drone-body.png";
+import propeller from "../assets/loading/propeller.png";
 
 export default function Loading() {
   return (
     <div className={classes.loading}>
-      <p>Ładowanie</p>
+      <div className={classes["loading-box"]}>
+        <div className={`${classes["drone-propeller"]} ${classes["top-left"]}`}>
+          <img src={propeller} alt="Drone's propeller - top left" />
+        </div>
+        <div
+          className={`${classes["drone-propeller"]} ${classes["top-right"]}`}
+        >
+          <img src={propeller} alt="Drone's propeller - top right" />
+        </div>
+        <div className={classes["drone-body"]}>
+          <img src={droneBody} alt="Drone's body" />
+        </div>
+        <div
+          className={`${classes["drone-propeller"]} ${classes["bottom-left"]}`}
+        >
+          <img src={propeller} alt="Drone's propeller - bottom left" />
+        </div>
+        <div
+          className={`${classes["drone-propeller"]} ${classes["bottom-right"]}`}
+        >
+          <img src={propeller} alt="Drone's propeller - bottom right" />
+        </div>
+      </div>
+      <div className={classes["loading-text"]}>Ładowanie</div>
     </div>
   );
 }
