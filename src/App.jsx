e0eef0637942b9c1f8893123ebf4882d48ac39af
Michal <michal@drontrzebownisko.pl>
2024-02-28 12:06:18 +0100

    Initial preparation of the project structure.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Loading from "./components/Loading.jsx";
+
+function App() {
+  return (
+    <>
+      <Loading />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import classes from "./Loading.module.scss";
+
+export default function Loading() {
+  return (
+    <div className={classes.loading}>
+      <p>Ładowanie</p>
+    </div>
+  );
+}
