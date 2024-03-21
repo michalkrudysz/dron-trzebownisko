f193bda71e26168dd9d7445810cb429231a2261e
Michal <michal@drontrzebownisko.pl>
2024-03-21 14:24:49 +0100

    Added company reference images and placed them in our 'mock' database. Also included in the company object is a key that stores a variable for the image. Additionally, removed Helmet for SEO purposes due to error generation.

--- data/references.js
+++ data/references.js
@@ -1,11 +1,19 @@
 import gminaTrzebownisko from "../src/assets/company-logos/gmina-trzebownisko.png";
+import gminaTrzebowniskoReference from "../src/assets/company-logos/gmina-trzebownisko-reference.jpg";
 import waran from "../src/assets/company-logos/waran.png";
+import waranReference from "../src/assets/company-logos/waran-reference.jpg";
 import innoreg from "../src/assets/company-logos/innoreg.png";
+import innoregReference from "../src/assets/company-logos/innoreg-reference.jpg";
 import tubisFilm from "../src/assets/company-logos/tubis-film.png";
+import tubisFilmReference from "../src/assets/company-logos/tubis-film-reference.jpg";
 import visualArtStudio from "../src/assets/company-logos/visual-art-studio.png";
+import visualArtStudioReference from "../src/assets/company-logos/visual-art-studio-reference.jpg";
 import newVentures from "../src/assets/company-logos/new-ventures.png";
+import newVenturesReference from "../src/assets/company-logos/new-ventures-reference.jpg";
 import kamko from "../src/assets/company-logos/kamko.png";
+import kamkoReference from "../src/assets/company-logos/kamko-reference.jpg";
 import videoMat from "../src/assets/company-logos/videomat.png";
+import videoMatReference from "../src/assets/company-logos/videomat-reference.jpg";
 
 const REFERENCES = [
   {
@@ -14,6 +22,7 @@ const REFERENCES = [
     companyName: "Urząd Gminy Trzebownisko",
     descriptionPL: `"Na szczególne uznanie zasługuje stworzenie w 2018 roku spotu promocyjnego dla Gminy, dostępnego na platformie YouTube..."`,
     descriptionEN: `Special recognition deserves the creation of a promotional video for the Municipality in 2018, available on the YouTube platform...`,
+    referenceImage: gminaTrzebowniskoReference,
   },
   {
     id: 1,
@@ -21,6 +30,7 @@ const REFERENCES = [
     companyName: "Waran Regały",
     descriptionPL: `"Pan Michał reagował na bieżąco na wszelkie nasze sugestie dotyczące projektu, jednocześnie sam wykazywał się dużą kreatywnością..."`,
     descriptionEN: `Mr. Michał reacted promptly to all our suggestions regarding the project, while also showing great creativity...`,
+    referenceImage: waranReference,
   },
   {
     id: 2,
@@ -28,6 +38,7 @@ const REFERENCES = [
     companyName: "Innoreg Sp. z o.o.",
     descriptionPL: `"Pan Michał wykazał się nie tylko zaangażowaniem, ale również strategicznym myśleniem w zakresie logistyki projektu..."`,
     descriptionEN: `Mr. Michał demonstrated not only commitment but also strategic thinking in terms of the project's logistics...`,
+    referenceImage: innoregReference,
   },
   {
     id: 3,
@@ -35,6 +46,7 @@ const REFERENCES = [
     companyName: "Tubis Film",
     descriptionPL: `"[...] Tworząc filmy, które konsekwentnie opowiadały historie par młodych, równocześnie podkreślając ich indywidualny charakter."`,
     descriptionEN: `[...] Creating videos that consistently told the stories of young couples, simultaneously emphasizing their individual character.`,
+    referenceImage: tubisFilmReference,
   },
   {
     id: 4,
@@ -42,6 +54,7 @@ const REFERENCES = [
     companyName: "Visual ART Studio",
     descriptionPL: `"Dzięki wrażliwości artystycznej oraz technicznym umiejętnościom, filmy stworzone przez niego nie były tylko zwykłym montażem scen..."`,
     descriptionEN: `Thanks to the artistic sensitivity and technical skills, the films created by him were not just ordinary scene montages...`,
+    referenceImage: visualArtStudioReference,
   },
   {
     id: 5,
@@ -49,6 +62,7 @@ const REFERENCES = [
     companyName: "New Ventures Sp. z o.o.",
     descriptionPL: `"Rekomendujemy współpracę z Panem Michałem Krudyszem, uważając go za osobę godną zaufania oraz niezawodnego partnera."`,
     descriptionEN: `We recommend cooperating with Mr. Michał Krudysz, considering him a trustworthy person and a reliable partner.`,
+    referenceImage: newVenturesReference,
   },
   {
     id: 6,
@@ -56,6 +70,7 @@ const REFERENCES = [
     companyName: "F.H.U. Kamko",
     descriptionPL: `"Oprócz filmowania, Pan Michał Krudysz podjął się również odpowiedzialności za postprodukcję. Jego dbałość o odpowiedni dobór ścieżki dźwiękowej..."`,
     descriptionEN: `In addition to filming, Mr. Michał Krudysz also took on the responsibility for post-production. His attention to the proper selection of the soundtrack...`,
+    referenceImage: kamkoReference,
   },
   {
     id: 7,
@@ -63,6 +78,7 @@ const REFERENCES = [
     companyName: "Videomat",
     descriptionPL: `"[...] Pan Michał przekształcał materiały w taki sposób, aby kolorystyka dodatkowo podkreślała i uzupełniała przedstawianą historię."`,
     descriptionEN: `[...] Mr. Michał transformed the materials in such a way that the color scheme additionally emphasized and complemented the presented story.`,
+    referenceImage: videoMatReference,
   },
 ];
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-import { Helmet } from "react-helmet";
 import { useLanguage } from "../store/language/languageContext";
 import REFERENCES from "../../data/references";
 import HOME_PAGE_CONTENT from "../../data/homePageContent";
@@ -9,31 +8,6 @@ import ReferenceBox from "./ReferenceBox";
 import ModalReference from "./ModalReference";
 
 const contentByLanguage = HOME_PAGE_CONTENT;
-
-const seo = {
-  title:
-    "Dron Trzebownisko - Profesjonalne Zdjęcia Lotnicze Gminy | Michał Krudysz",
-  description:
-    "Odkryj niezrównane piękno gminy Trzebownisko z nowej perspektywy dzięki profesjonalnym zdjęciom lotniczym. Michał Krudysz, ekspert w dziedzinie fotografii dronowej, przedstawia unikalne widoki Jasionki, Łąki, Łukawca, Nowej Wsi, Stobiernej, Tajęciny, Terliczki, Trzebowniska, Wólki Podleśnej, Zaczernia i więcej. Dołącz do naszej społeczności i odkrywaj z nami każdy zakątek gminy, dzięki interaktywnej mapie i bogatej galerii zdjęć. Sprawdź również nasze portfolio, które obejmuje zadziwiające fotografie z różnych miejsc Europy i referencje od renomowanych firm, potwierdzające najwyższą jakość usług.",
-  keywords:
-    "dron trzebownisko, zdjęcia lotnicze, fotografie dronowe, Michał Krudysz, portfolio fotograficzne, gmina Trzebownisko, widoki z drona, odkrywanie gminy, sołectwa Jasionka, Łąka, Łukawiec, Nowa Wieś, Stobierna, Tajęcina, Terliczka, Trzebownisko, Wólka Podleśna, Zaczernie, turystyka, promocja gminy, fotografie Europy",
-  robots: "index, follow",
-  canonical: "https://drontrzebownisko.pl",
-  og: {
-    site_name: "Dron Trzebownisko",
-    type: "website",
-    image: "https://drontrzebownisko.pl/path_to_your_social_media_image.jpg",
-    locale: "pl_PL",
-  },
-  twitter: {
-    card: "summary_large_image",
-    // creator: "@your_twitter_handle",
-    title:
-      "Dron Trzebownisko - Profesjonalne Zdjęcia Lotnicze Gminy | Michał Krudysz",
-    description:
-      "Odkryj niezrównane piękno gminy Trzebownisko z nowej perspektywy...",
-  },
-};
 
 const getRandomIndexes = (length, count) => {
   let indexes = [];
@@ -70,24 +44,6 @@ export default function HomePage() {
 
   return (
     <>
-      <Helmet>
-        <title>{seo.title}</title>
-        <meta name="description" content={seo.description} />
-        <meta name="keywords" content={seo.keywords} />
-        <meta name="robots" content={seo.robots} />
-        <link rel="canonical" href={seo.canonical} />
-        <meta property="og:title" content={seo.title} />
-        <meta property="og:description" content={seo.description} />
-        <meta property="og:image" content={seo.og.image} />
-        <meta property="og:type" content={seo.og.type} />
-        <meta property="og:site_name" content={seo.og.site_name} />
-        <meta property="og:locale" content={seo.og.locale} />
-        <meta name="twitter:card" content={seo.twitter.card} />
-        <meta name="twitter:title" content={seo.twitter.title} />
-        <meta name="twitter:description" content={seo.twitter.description} />
-        <meta name="twitter:creator" content={seo.twitter.creator} />
-      </Helmet>
-
       <div className={classes["home-page"]}>
         <section className={classes.section}>
           <div className={classes["left-container"]}>
