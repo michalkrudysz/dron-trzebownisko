db35cc5ac9c8b1d7330350a39d478bf3934d0b47
Michal <michal@drontrzebownisko.pl>
2024-03-21 12:16:12 +0100

    Styled a modal with areference for the primary resolution.

--- src/components/ModalReference.jsx
+++ src/components/ModalReference.jsx
@@ -1,13 +1,19 @@
 import classes from "./ModalReference.module.scss";
 import ReactDOM from "react-dom";
 import innoReg from "../assets/company-logos/innoreg-reference.jpg";
+import buttonClose from "../assets/close-reference.png";
 
 const ModalReference = () => {
   return ReactDOM.createPortal(
-    <div className={classes["modal-reference"]}>
-      <div className={classes["modal-reference__content"]}></div>
-      <img className={classes["reference"]} src={innoReg} alt="reference" />
-    </div>,
+    <>
+      <img className={classes["button-close"]} src={buttonClose} alt="close" />
+      <div className={classes["modal-reference"]}>
+        <div className={classes["modal-reference__content"]}>
+          <img className={classes["reference"]} src={innoReg} alt="reference" />
+        </div>
+      </div>
+      <div className={classes["shadow"]}></div>
+    </>,
     document.getElementById("modal-reference")
   );
 };
