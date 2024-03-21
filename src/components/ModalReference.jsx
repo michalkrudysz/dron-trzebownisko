013a9ab6ba50940c2cf7499ddd6c22b4dd2ced37
Michal <michal@drontrzebownisko.pl>
2024-03-21 12:32:07 +0100

    Implemented the logic for closing a modal that displays a reference.

--- src/components/ModalReference.jsx
+++ src/components/ModalReference.jsx
@@ -2,11 +2,32 @@ import classes from "./ModalReference.module.scss";
 import ReactDOM from "react-dom";
 import innoReg from "../assets/company-logos/innoreg-reference.jpg";
 import buttonClose from "../assets/close-reference.png";
+import { useState, forwardRef, useImperativeHandle } from "react";
 
-const ModalReference = () => {
+const ModalReference = forwardRef((props, ref) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  useImperativeHandle(ref, () => ({
+    openModal() {
+      setIsOpen(true);
+    },
+  }));
+
+  if (!isOpen) {
+    return null;
+  }
+  const handleCloseClick = () => {
+    setIsOpen(false);
+  };
+
   return ReactDOM.createPortal(
     <>
-      <img className={classes["button-close"]} src={buttonClose} alt="close" />
+      <img
+        className={classes["button-close"]}
+        src={buttonClose}
+        alt="close"
+        onClick={handleCloseClick}
+      />
       <div className={classes["modal-reference"]}>
         <div className={classes["modal-reference__content"]}>
           <img className={classes["reference"]} src={innoReg} alt="reference" />
@@ -16,6 +37,8 @@ const ModalReference = () => {
     </>,
     document.getElementById("modal-reference")
   );
-};
+});
+
+ModalReference.displayName = "ModalReference";
 
 export default ModalReference;
