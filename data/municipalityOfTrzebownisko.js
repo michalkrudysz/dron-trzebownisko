eeb421146b029907b24b8a20052a198217d9ed95
Michal <michal@drontrzebownisko.pl>
2024-03-29 16:44:15 +0100

    Added additional resources from another locality.

--- data/municipalityOfTrzebownisko.js
+++ data/municipalityOfTrzebownisko.js
@@ -1,5 +1,21 @@
-import jasionkaChurch from "../src/assets/municipality-of-trzebownisko/jasionka/jasionka-church-2017.png";
-import jasionkaChurch2 from "../src/assets/municipality-of-trzebownisko/jasionka/jasionka-church-2017-2.png";
+import jasionkaChurch2017 from "../src/assets/municipality-of-trzebownisko/jasionka/jasionka-church-2017.png";
+import jasionkaChurch2017_2 from "../src/assets/municipality-of-trzebownisko/jasionka/jasionka-church-2017-2.png";
+import jasionkaAirport2018 from "../src/assets/municipality-of-trzebownisko/jasionka/airport-2018.png";
+import jasionkaAirport2018_2 from "../src/assets/municipality-of-trzebownisko/jasionka/airport-2018-2.png";
+import jasionkaAirport2018_3 from "../src/assets/municipality-of-trzebownisko/jasionka/airport-2018-3.png";
+import jasionkaAirport2018_4 from "../src/assets/municipality-of-trzebownisko/jasionka/airport-2018-4.png";
+import lakaChurch2017 from "../src/assets/municipality-of-trzebownisko/laka/laka-church-2017.png";
+import lakaChurch2017_2 from "../src/assets/municipality-of-trzebownisko/laka/laka-church-2017-2.png";
+import lakaChurch2017_3 from "../src/assets/municipality-of-trzebownisko/laka/laka-church-2017-3.png";
+import nursingHome2017 from "../src/assets/municipality-of-trzebownisko/laka/nursing-home-2017.png";
+import nursingHome2017_2 from "../src/assets/municipality-of-trzebownisko/laka/nursing-home-2017-2.png";
+import oldGranary2017 from "../src/assets/municipality-of-trzebownisko/laka/old-granary-2017.png";
+import oldGranary2017_2 from "../src/assets/municipality-of-trzebownisko/laka/old-granary-2017-2.png";
+import oldGranary2017_3 from "../src/assets/municipality-of-trzebownisko/laka/old-granary-2017-3.png";
+import school2017 from "../src/assets/municipality-of-trzebownisko/laka/school-2017.png";
+import school2017_2 from "../src/assets/municipality-of-trzebownisko/laka/school-2017-2.png";
+import church2019 from "../src/assets/municipality-of-trzebownisko/laka/church-2019.png";
+import church2019_2 from "../src/assets/municipality-of-trzebownisko/laka/church-2019-2.png";
 
 const MUNICIPALITY_OF_TRZEBOWNISKO = [
   {
@@ -10,7 +26,7 @@ const MUNICIPALITY_OF_TRZEBOWNISKO = [
       "Jasionka is a small town located in south-eastern Poland, in the Podkarpackie Voivodeship, near Rzeszów. It is known primarily thanks to its developing Rzeszów-Jasionka Airport, which is an important communication point for the region, offering domestic and international connections. Besides its transport role, Jasionka attracts attention thanks to the modern Exhibition and Congress Centre, which is the venue for many significant events, trade fairs, conferences, and business meetings.",
     Materials: [
       {
-        img: jasionkaChurch,
+        img: jasionkaAirport2018,
         alt: "zdjęcie portu lotniczego",
         altEN: "photo of the airport",
         dateOfPhoto: 2017,
@@ -20,7 +36,175 @@ const MUNICIPALITY_OF_TRZEBOWNISKO = [
           "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
       },
       {
-        img: jasionkaChurch2,
+        img: jasionkaAirport2018_2,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: jasionkaAirport2018_3,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: jasionkaAirport2018_4,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: jasionkaChurch2017,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: jasionkaChurch2017_2,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+    ],
+  },
+  {
+    name: "Łąka",
+    descriptionPL: `Łąka to malownicza wieś położona w południowo-wschodniej Polsce, w województwie podkarpackim, wchodząca w skład administracyjny Gminy Trzebownisko, w powiecie rzeszowskim. Wieś jest usytuowana około 4 kilometry na wschód od Trzebowniska i 9 kilometrów na północny wschód od Rzeszowa, co czyni ją dobrze skomunikowaną z większymi ośrodkami urbanistycznymi, zachowując jednocześnie swój wiejski charakter i spokój. Łąka jest społecznością o bogatym życiu kulturalnym i społecznym, w której działa wiele organizacji, w tym Ochotnicza Straż Pożarna, Ludowy Klub Sportowy, czy Stowarzyszenie „Wszyscy Razem”.`,
+    descriptionEN:
+      "Jasionka is a small town located in south-eastern Poland, in the Podkarpackie Voivodeship, near Rzeszów. It is known primarily thanks to its developing Rzeszów-Jasionka Airport, which is an important communication point for the region, offering domestic and international connections. Besides its transport role, Jasionka attracts attention thanks to the modern Exhibition and Congress Centre, which is the venue for many significant events, trade fairs, conferences, and business meetings.",
+    Materials: [
+      {
+        img: lakaChurch2017,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: lakaChurch2017_2,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: lakaChurch2017_3,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: church2019,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: church2019_2,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: nursingHome2017,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: nursingHome2017_2,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: oldGranary2017,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: oldGranary2017_2,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: oldGranary2017_3,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: school2017,
+        alt: "zdjęcie portu lotniczego",
+        altEN: "photo of the airport",
+        dateOfPhoto: 2017,
+        curiosityPL:
+          "Port lotniczy Rzeszów-Jasionka był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa.",
+        curiosityEN:
+          "Rzeszów-Jasionka Airport was the first in Poland to introduce contactless security gates.",
+      },
+      {
+        img: school2017_2,
         alt: "zdjęcie portu lotniczego",
         altEN: "photo of the airport",
         dateOfPhoto: 2017,
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -13,46 +13,18 @@ export default function Location({ location }) {
           <div className={classes.title}>{location.name}</div>
         </div>
         <div className={classes.right}>
-          <div className={classes.description}>
-            Jasionka to niewielka miejscowość położona w południowo-wschodniej
-            Polsce, w województwie podkarpackim, niedaleko Rzeszowa. Jest znana
-            przede wszystkim dzięki swojemu rozwijającemu się portowi lotniczemu
-            Rzeszów-Jasionka, który stanowi ważny punkt komunikacyjny dla
-            regionu, oferując połączenia krajowe i międzynarodowe. Oprócz roli
-            transportowej, Jasionka przyciąga uwagę dzięki nowoczesnemu Centrum
-            Wystawienniczo-Kongresowemu, które jest miejscem wielu znaczących
-            wydarzeń, targów, konferencji i spotkań biznesowych.
-          </div>
+          <div className={classes.description}>{location.descriptionPL}</div>
         </div>
       </div>
       <div className={classes.bottom}>
         <div className={classes.header}>Dostępne materiały</div>
         <div className={classes.materials}>
           <ul>
-            <li>
-              <img
-                src="https://www.rzeszowairport.pl/images/news/main/136.jpg"
-                alt="zdjęcie"
-              />
-            </li>
-            <li>
-              <img
-                src="https://www.rzeszowairport.pl/images/news/main/136.jpg"
-                alt="zdjęcie"
-              />
-            </li>
-            <li>
-              <img
-                src="https://www.rzeszowairport.pl/images/news/main/136.jpg"
-                alt="zdjęcie"
-              />
-            </li>
-            <li>
-              <img
-                src="https://www.rzeszowairport.pl/images/news/main/136.jpg"
-                alt="zdjęcie"
-              />
-            </li>
+            {location.Materials.map((material, index) => (
+              <li key={index}>
+                <img src={material.img} alt="zdjęcie" />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
