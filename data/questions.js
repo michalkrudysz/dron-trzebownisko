ad1661038e1e429285049d482049d9138cbbb1e1
Michal <michal@drontrzebownisko.pl>
2024-04-02 16:16:03 +0200

    Created mock data for a quiz, which were imported into the Quiz component. Developed an initial HTML structure and implemented logic for answering questions and navigating to the next one.

---
+++ data/questions.js
@@ -0,0 +1,66 @@
+import jasionkaAirport2018_2 from "../src/assets/municipality-of-trzebownisko/jasionka/airport-2018-2.png";
+import jasionkaAirport2018_3 from "../src/assets/municipality-of-trzebownisko/jasionka/airport-2018-3.png";
+import jasionkaChurch2017 from "../src/assets/municipality-of-trzebownisko/jasionka/jasionka-church-2017.png";
+
+export default [
+  {
+    id: "q1",
+    image: jasionkaAirport2018_3,
+    text: "Który z wymienionych obiektów został otwarty w Jasionce w 2016 roku?",
+    alt: "Opis",
+    answers: [
+      "Centrum Wystawienniczo-Kongresowe",
+      "Stadion sportowy",
+      "Szkolne lotnisko Ośrodka Kształcenia Lotniczego Politechniki Rzeszowskiej",
+      "Port lotniczy Rzeszów-Jasionka",
+    ],
+  },
+  {
+    id: "q2",
+    image: jasionkaAirport2018_2,
+    text: "Która z postaci historycznych odwiedziła Jasionkę w 1991 roku?",
+    alt: "Opis",
+    answers: [
+      "Papież Jan Paweł II",
+      "Prezydent USA, Joe Biden",
+      "Św. Jan z Dukli",
+      "Andrzej Duda, Prezydent Polski",
+    ],
+  },
+  {
+    id: "q3",
+    image: jasionkaChurch2017,
+    text: "Jakie wydarzenie miało miejsce w kościele w Jasionce 10 czerwca 1997 roku?",
+    alt: "Opis",
+    answers: [
+      "Ojciec Święty Jan Paweł II poświęcił kamień węgielny dla budującego się kościoła",
+      "Pierwsza msza w nowo wybudowanym kościele",
+      "Kanonizacja św. Jana z Dukli",
+      "Spotkanie z żołnierzami z 82. Dywizji Powietrznodesantowej",
+    ],
+  },
+  {
+    id: "q4",
+    image: jasionkaAirport2018_2,
+    text: "Co jest wyjątkowego w porcie lotniczym Rzeszów-Jasionka w Polsce?",
+    alt: "Opis",
+    answers: [
+      "Był pierwszym w Polsce, który wprowadził bezkontaktowe bramki bezpieczeństwa",
+      "Jest jedynym portem lotniczym w Polsce z własną szkołą lotniczą",
+      "Posiada najdłuższy pas startowy w Polsce",
+      "Znajduje się najbliżej granicy z Ukrainą",
+    ],
+  },
+  {
+    id: "q5",
+    image: jasionkaAirport2018_2,
+    text: "Jakie działania podjęła armia amerykańska w Jasionce w 2022 roku?",
+    alt: "Opis",
+    answers: [
+      "Utworzyła swoją bazę w Centrum Wystawienniczo-Kongresowym",
+      "Zorganizowała wspólne ćwiczenia z polskim wojskiem",
+      "Przeprowadziła operację ewakuacyjną obywateli amerykańskich",
+      "Zbudowała nowy terminal lotniczy",
+    ],
+  },
+];
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,38 @@
 import classes from "./Quiz.module.scss";
+import { useState } from "react";
+import QUESTIONS from "../../data/questions";
 
 export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleAnswerClick(answer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, answer];
+    });
+  }
+
   return (
     <div className={classes.quiz}>
-      <h1>Quiz</h1>
+      <div className={classes.image}>
+        <img
+          src={QUESTIONS[activeQuestionIndex].image}
+          alt={QUESTIONS[activeQuestionIndex].alt}
+        />
+      </div>
+      <div className={classes.question}>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul className={classes.answers}>
+          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+            <li key={index} className={classes.answer}>
+              <button onClick={() => handleAnswerClick(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
