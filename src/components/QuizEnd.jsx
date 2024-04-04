497653c00e47142f01b864b3690a2211c28b39d5
Michal <michal@drontrzebownisko.pl>
2024-04-04 14:19:34 +0200

    The QuizEnd component was styled for visual appeal and user interaction enhancement.

--- src/components/QuizEnd.jsx
+++ src/components/QuizEnd.jsx
@@ -1,30 +1,52 @@
+import classes from "./QuizEnd.module.scss";
+
 export default function QuizEnd({ userAnswers, questions }) {
   const totalQuestions = questions.length;
   const correctAnswers = userAnswers.filter(
     (answer, index) => answer === questions[index].answers[0]
   ).length;
   const skippedAnswers = userAnswers.filter((answer) => answer === null).length;
-  const incorrectAnswers = userAnswers.length - correctAnswers - skippedAnswers;
+  const incorrectAnswers = totalQuestions - correctAnswers - skippedAnswers;
 
   const correctPercentage = (correctAnswers / totalQuestions) * 100;
   const skippedPercentage = (skippedAnswers / totalQuestions) * 100;
   const incorrectPercentage = (incorrectAnswers / totalQuestions) * 100;
 
   return (
-    <div>
-      <h2>Podsumowanie</h2>
-      <p>
-        <strong>Poprawne odpowiedzi:</strong> {correctAnswers} (
-        {correctPercentage.toFixed(2)}%)
-      </p>
-      <p>
-        <strong>Pominięte odpowiedzi:</strong> {skippedAnswers} (
-        {skippedPercentage.toFixed(2)}%)
-      </p>
-      <p>
-        <strong>Błędne odpowiedzi:</strong> {incorrectAnswers} (
-        {incorrectPercentage.toFixed(2)}%)
-      </p>
-    </div>
+    <>
+      <div className={classes.summary}>
+        <h1 className={classes.title}>
+          <span className={classes.strong}>Brawo!</span> Ukończyłeś test
+          dotyczący wiedzy o Gminie Trzebownisko!
+        </h1>
+        <h2 className={classes["summary-results"]}>Podsumowanie wyników</h2>
+        <div className={classes.results}>
+          <p className={classes.paragraph}>
+            <strong className={classes.text}>Poprawne odpowiedzi:</strong>{" "}
+            <span className={classes.score}>
+              {correctAnswers} ({correctPercentage.toFixed(2)}%)
+            </span>
+          </p>
+          <p className={classes.paragraph}>
+            <strong className={classes.text}>Pominięte odpowiedzi:</strong>{" "}
+            <span className={classes.score}>
+              {skippedAnswers} ({skippedPercentage.toFixed(2)}%)
+            </span>
+          </p>
+          <p className={classes.paragraph}>
+            <strong className={classes.text}>Niepoprawne odpowiedzi:</strong>{" "}
+            <span className={classes.score}>
+              {incorrectAnswers} ({incorrectPercentage.toFixed(2)}%)
+            </span>
+          </p>
+        </div>
+        <div className={classes.buttons}>
+          <button className={classes.button}>Powrót do strony głównej</button>
+          <button className={classes.button}>Powtórz test</button>
+        </div>
+      </div>
+
+      <div className={classes["shadow"]}></div>
+    </>
   );
 }
