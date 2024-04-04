a0525f6ec09a22e22125b7527485b13da07eba1c
Michal <michal@drontrzebownisko.pl>
2024-04-04 12:54:25 +0200

    Refactored the state logic of responses into the Question component, eliminated unnecessary props, and introduced a new state for handling answer selection.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,38 +1,50 @@
+import { useState } from "react";
 import classes from "./Quiz.module.scss";
 import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
+import QUESTIONS from "../../data/questions";
 
 export default function Question({
-  questionText,
-  answers,
   onSelectAnswer,
-  activeQuestionIndex,
-  handleSkipAnswer,
-  answerState,
-  userAnswers,
+  questionIndex,
   imageSrc,
   imageAlt,
 }) {
+  const [answer, setAnswer] = useState({ selectedAnswer: "", isCorrect: null });
+
+  const handleSelectAnswer = (answerText) => {
+    setAnswer({ selectedAnswer: answerText, isCorrect: null });
+    setTimeout(() => {
+      const isCorrect = answerText === QUESTIONS[questionIndex].answers[0];
+      setAnswer((prev) => ({ ...prev, isCorrect }));
+      onSelectAnswer(answerText);
+    }, 1000);
+  };
+
   return (
     <div className={classes.quiz}>
       <div className={classes.timer}>
         <QuestionTimer
-          key={activeQuestionIndex}
           timeout={10000}
-          onTimeout={handleSkipAnswer}
+          onTimeout={() => handleSelectAnswer(null)}
         />
       </div>
       <div className={classes.image}>
         <img src={imageSrc} alt={imageAlt} />
       </div>
       <div className={classes.question}>
-        <h2 className={classes.header}>{questionText}</h2>
+        <h2 className={classes.header}>{QUESTIONS[questionIndex].text}</h2>
         <Answers
-          key={activeQuestionIndex}
-          answers={answers}
-          selectedAnswer={userAnswers[userAnswers.length - 1]}
-          answerState={answerState}
-          onSelect={onSelectAnswer}
+          answers={QUESTIONS[questionIndex].answers}
+          selectedAnswer={answer.selectedAnswer}
+          answerState={
+            answer.isCorrect === null
+              ? ""
+              : answer.isCorrect
+              ? "correct"
+              : "wrong"
+          }
+          onSelect={handleSelectAnswer}
         />
       </div>
     </div>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,40 +1,16 @@
 import classes from "./Quiz.module.scss";
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import QUESTIONS from "../../data/questions";
 import Question from "./Question";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
   const quizFinished = activeQuestionIndex === QUESTIONS.length;
 
-  const handleAnswerClick = useCallback(
-    (answer) => {
-      setAnswerState("answered");
-      setUserAnswers((prevUserAnswers) => [...prevUserAnswers, answer]);
-
-      setTimeout(() => {
-        if (answer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correctly");
-        } else {
-          setAnswerState("incorrectly");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
-
-  const handleSkipAnswer = useCallback(
-    () => handleAnswerClick(null),
-    [handleAnswerClick]
-  );
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+  };
 
   if (quizFinished) {
     return <div className={classes.summary}>Koniec quizu.</div>;
@@ -44,12 +20,10 @@ export default function Quiz() {
     <>
       <Question
         questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        onSelectAnswer={handleAnswerClick}
+        onSelectAnswer={handleSelectAnswer}
         activeQuestionIndex={activeQuestionIndex}
-        handleSkipAnswer={handleSkipAnswer}
-        answerState={answerState}
-        userAnswers={userAnswers}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        questionIndex={activeQuestionIndex}
         imageSrc={QUESTIONS[activeQuestionIndex].image}
         imageAlt={QUESTIONS[activeQuestionIndex].alt}
       />
