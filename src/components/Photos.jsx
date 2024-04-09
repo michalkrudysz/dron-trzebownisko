98ff77ca52f27486d7822a82d9cf9548ce267218
Michal <michal@drontrzebownisko.pl>
2024-04-09 12:44:44 +0200

    Also integrated the photo modal with the section where all photos are displayed.

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,12 +1,24 @@
 import classes from "./Photos.module.scss";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useLanguage } from "../store/language/languageContext";
+import PhotoModal from "./PhotoModal";
+import { useRef } from "react";
+import { useState } from "react";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 export default function Photos({ photos }) {
   const { language } = useLanguage();
+  const [currentPhoto, setCurrentPhoto] = useState(null);
+  const photoModal = useRef();
+
+  const openPhoto = (photo) => {
+    setCurrentPhoto(photo);
+    photoModal.current.openModal();
+  };
+
   return (
     <>
+      <PhotoModal ref={photoModal} imageData={currentPhoto} />
       <div className={classes.top}>
         <h1 className={classes.title}>Zdjęcia warte uwagi</h1>
       </div>
@@ -15,6 +27,7 @@ export default function Photos({ photos }) {
           <div key={index} className={classes["photo-box"]}>
             <div className={classes.photo}>
               <LazyLoadImage
+                onClick={() => openPhoto(photo)}
                 src={photo.imgUrl}
                 alt={language === "PL" ? photo.altPL : photo.altEN}
                 effect="blur"
